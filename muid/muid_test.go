9e4e8f875792ebd4e770392a16b75c0ae19d73e9
Gabriel Willen <91307534+gabewillen@users.noreply.github.com>
2025-04-29 12:18:24 -0500

    V2 (#18)
    * Refactor Dispatch calls in After and Every functions to use hsm.Context() for improved context handling. This change ensures that the correct context is utilized during event dispatching, enhancing the reliability of state machine operations.
    
    * Update version to v1.9.1.3 in version.go to reflect recent changes.
    
    * Refactor Trace structure and enhance HSM event handling
    
    - Updated the Trace struct to use a pointer for the mutex, improving memory efficiency.
    - Introduced new test cases for HSM event handling, including WaitForEntry, WaitForExit, and WaitForDispatch, to validate state transitions and event dispatching.
    - Refactored the HSM instance interface to replace the wait method with listeners for better event management.
    - Enhanced the transition logic to notify listeners on entry, exit and dispatch
    
    * Update version to v1.9.2 in version.go to reflect recent changes.
    
    * Remove deprecated files and update module path to v2 in go.mod and related imports. Update version to v2.0.0 in version.go to reflect major changes.
    
    * Refactor HSM event handling and introduce snapshot functionality
    
    - Removed the Done channel from Event struct and updated related event dispatching logic to enhance clarity and usability.
    - Introduced a new Snapshot struct to capture the state machine's current state, including ID, qualified name, state, and queue length.
    - Updated tests to reflect changes in event handling and added a new TestSnapshot to validate snapshot functionality.
    - Enhanced README documentation to include new features and usage examples for snapshotting and event handling improvements.

--- muid/muid_test.go
+++ muid/muid_test.go
@@ -1,22 +1,16 @@
-package muid_test
+package muid
 
 import (
-	"strconv"
 	"testing"
-
-	"github.com/godruoyi/go-snowflake"
-	"github.com/google/uuid"
-	"github.com/oklog/ulid/v2"
-	"github.com/runpod/hsm/muid"
 )
 
 func TestMUID(t *testing.T) {
 	const total = 1_000_000
-	muids := make(map[muid.MUID]bool)
-	ch := make(chan muid.MUID, total)
+	muids := make(map[MUID]bool)
+	ch := make(chan MUID, total)
 	for i := 0; i < total; i++ {
 		go func() {
-			muid := muid.Make()
+			muid := Make()
 			ch <- muid
 		}()
 	}
@@ -31,118 +25,11 @@ func TestMUID(t *testing.T) {
 
 func TestMUIDStringLength(t *testing.T) {
 	const total = 1_000_000
-	length := len(muid.Make().String())
+	length := len(Make().String())
 	for i := 0; i < total; i++ {
-		muid := muid.Make()
+		muid := Make()
 		if len(muid.String()) != length {
 			t.Fatalf("muid string length is not %d: %d", length, len(muid.String()))
 		}
 	}
-}
-
-func BenchmarkIds(b *testing.B) {
-	b.Run("uuid", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			uuid.NewV7()
-		}
-	})
-	b.Run("uuid_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				uuid.NewV7()
-			}
-		})
-	})
-	b.Run("uuid_string", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			uuid, _ := uuid.NewV7()
-			_ = uuid.String()
-		}
-	})
-	b.Run("uuid_string_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				uuid, _ := uuid.NewV7()
-				_ = uuid.String()
-			}
-		})
-	})
-	b.Run("snowflake", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			snowflake.ID()
-		}
-	})
-	b.Run("snowflake_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				snowflake.ID()
-			}
-		})
-	})
-	b.Run("snowflake_string", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			id := snowflake.ID()
-			_ = strconv.FormatUint(id, 10)
-		}
-	})
-	b.Run("snowflake_string_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				id := snowflake.ID()
-				_ = strconv.FormatUint(id, 10)
-			}
-		})
-	})
-	b.Run("ulid", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			ulid.Make()
-		}
-	})
-	b.Run("ulid_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				ulid.Make()
-			}
-		})
-	})
-	b.Run("ulid_string", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			id := ulid.Make()
-			_ = id.String()
-		}
-	})
-	b.Run("ulid_string_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				id := ulid.Make()
-				_ = id.String()
-			}
-		})
-	})
-	b.Run("muid", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			muid.Make()
-		}
-	})
-	b.Run("muid_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				muid.Make()
-			}
-		})
-	})
-	b.Run("muid_string", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			id := muid.Make()
-			_ = id.String()
-		}
-	})
-	b.Run("muid_string_parallel", func(b *testing.B) {
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				id := muid.Make()
-				_ = id.String()
-			}
-		})
-	})
 }
